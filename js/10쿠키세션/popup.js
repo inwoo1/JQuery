3b856794fdb2a9ce4d9580ab187ab1822663395d
inwoo1 <coninwoo@gmail.com>
2022-03-08 16:00:47 +0900

    20220308 js

---
+++ js/10쿠키세션/popup.js
@@ -0,0 +1,25 @@
+//팝업쿠키생성함수    쿠키이름을 받습니다
+function createCookie(name) {
+
+    //쿠키 만료기간
+    var date = new Date();
+    date.setDate( date.getDate() + 1 );  // 1일 
+    //쿠키구문
+    var cookie = "";
+    cookie += name + "=true;";
+    cookie += "expires=" + date.toUTCString() + ";";
+    //쿠키생성
+    document.cookie = cookie;
+}
+
+//쿠키확인함수
+function getCookie(name) {
+
+    var cookies = document.cookie.split("; ");
+    for(var i = 0; i < cookies.length; i++){
+        if(cookies[i].indexOf(name) != -1){
+            return true;  //존재여부를 true로 반환
+        }
+    }
+
+}
